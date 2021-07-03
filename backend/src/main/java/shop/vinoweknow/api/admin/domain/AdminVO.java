674e0d517a37960ddaa5fe57a3c3d2133f35d18c
crstal0120 <gkdbs0120@gmail.com>
2021-07-03 16:38:40 +0900

    마이크로서비스 설정완료

---
+++ backend/src/main/java/shop/vinoweknow/api/admin/domain/AdminVO.java
@@ -0,0 +1,4 @@
+package shop.vinoweknow.api.admin.domain;
+
+public class AdminVO {
+}
---
+++ backend/src/main/java/shop/vinoweknow/api/test.java
@@ -0,0 +1,8 @@
+package shop.vinoweknow.api;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+
+}
